c2b778a491ec33a63c11c8de1ec04bd403018404
Ester Mabel <estermabel2@gmail.com>
2021-07-19 18:25:52 -0300

    face detection start

--- lib/pages/FaceDetection/face_detection_page.dart
+++ lib/pages/FaceDetection/face_detection_page.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:google_ml_kit/google_ml_kit.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:machine_learning/utils/helpers.dart';
 
 class FaceDetectionPage extends StatefulWidget {
@@ -16,6 +20,90 @@ class FaceDetectionPage extends StatefulWidget {
 }
 
 class _FaceDetectionPageState extends State<FaceDetectionPage> {
+  late FaceDetector faceDetector;
+  late List<Face> faces;
+  late ImagePicker imagePicker;
+  Future<File>? imageFile;
+  File? _image;
+  List<String> result = [];
+  var image;
+
+  @override
+  void initState() {
+    super.initState();
+    imagePicker = ImagePicker();
+    faceDetector = GoogleMlKit.vision.faceDetector(
+      FaceDetectorOptions(
+        enableClassification: true,
+        minFaceSize: 0.1,
+        mode: FaceDetectorMode.fast,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    faceDetector.close();
+  }
+
+  doFaceDetection() async {
+    result.clear();
+    final inputImage = InputImage.fromFile(_image!);
+    faces = await faceDetector.processImage(inputImage);
+    print(faces.length.toString() + " faces");
+    drawRectangleAroundFaces();
+    if (faces.length > 0) {
+      for (var face in faces) {
+        if (face.smilingProbability! > 0.5) {
+          result.add("Smiling");
+        } else {
+          result.add("Serious");
+        }
+      }
+    }
+  }
+
+  drawRectangleAroundFaces() async {
+    image = await _image!.readAsBytes();
+    image = await decodeImageFromList(image);
+    setState(() {
+      faces;
+      result;
+    });
+  }
+
+  _getImageFromCamera() async {
+    PickedFile? pickedFile = await imagePicker.getImage(
+      source: ImageSource.camera,
+    );
+
+    setState(() {
+      _image = pickedFile != null ? File(pickedFile.path) : _image;
+      if (_image != null) {
+        doFaceDetection();
+      }
+    });
+  }
+
+  _getImageFromGallery() async {
+    PickedFile? pickedFile = await imagePicker.getImage(
+      source: ImageSource.gallery,
+    );
+    setState(() {
+      _image = pickedFile != null ? File(pickedFile.path) : _image;
+      if (_image != null) {
+        doFaceDetection();
+      }
+    });
+  }
+
+  imageNull() {
+    if (image == null) {
+      return 200.0;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,8 +115,8 @@ class _FaceDetectionPageState extends State<FaceDetectionPage> {
         onPressed: () {
           Helpers.showCameraOptionsDialog(
             context: context,
-            // camera: _getImageFromCamera,
-            // gallery: _getImageFromGallery,
+            camera: _getImageFromCamera,
+            gallery: _getImageFromGallery,
             // live: _getLiveCamera,
           );
         },
@@ -40,11 +128,93 @@ class _FaceDetectionPageState extends State<FaceDetectionPage> {
           child: Center(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
-              children: [],
+              children: [
+                Container(
+                  width: imageNull(),
+                  height: imageNull(),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child:
+                      // isLive
+                      //     ? controller.value.isInitialized
+                      //         ? AspectRatio(
+                      //             aspectRatio: controller.value.aspectRatio,
+                      //             child: CameraPreview(controller),
+                      //           )
+                      //         : Icon(
+                      //             Icons.camera_alt,
+                      //             color: Colors.grey[500],
+                      //           ) :
+
+                      _image != null
+                          ? FittedBox(
+                              child: SizedBox(
+                                width: image.width.toDouble(),
+                                height: image.height.toDouble(),
+                                child: CustomPaint(
+                                  painter: FacePainter(
+                                    rect: faces,
+                                    imageFile: image,
+                                  ),
+                                ),
+                              ),
+                            )
+                          : Icon(
+                              Icons.camera_alt,
+                              color: Colors.grey[500],
+                            ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 20),
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    itemCount: result.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text(
+                          result[index],
+                          textAlign: TextAlign.center,
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
             ),
           ),
         ),
       ),
     );
+  }
+}
+
+class FacePainter extends CustomPainter {
+  List<Face>? rect;
+  var imageFile;
+  FacePainter({required this.rect, required this.imageFile});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    if (imageFile != null) {
+      canvas.drawImage(imageFile, Offset.zero, Paint());
+    }
+    Paint p = Paint();
+    p.color = Colors.red;
+    p.style = PaintingStyle.stroke;
+    p.strokeWidth = 4;
+
+    if (rect != null) {
+      for (Face rectangle in rect!) {
+        canvas.drawRect(rectangle.boundingBox, p);
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
   }
 }
